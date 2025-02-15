e403c229991242e788ea9b19f87d230896c683a2
yashmane987 <yashmane4045@gmail.com>
2025-02-15 11:16:17 +0530

    new

--- src/pages/ContactUspage.js
+++ src/pages/ContactUspage.js
@@ -6,10 +6,11 @@ const ContactUspage = () => {
       <div className="contact-box">
         <div className="left">
           <iframe
+            title="Google Maps Location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60952.88666490492!2d74.15605481785066!3d17.28873079662746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc182450c753bad%3A0xb3493b19ae7886ca!2sKarad%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1739523386037!5m2!1sen!2sin"
-            allowfullscreen=""
+            allowFullScreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
         <div className="right">
